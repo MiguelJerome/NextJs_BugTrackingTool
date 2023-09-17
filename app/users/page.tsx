64c8003e5077fcecea979ad931e992cfa6024e19
MiguelJerome <sharkmaycry@gmail.com>
2023-09-16 22:28:50 -0400

    add tablefor users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface User {
     id: number;
     name: string;
+    email: string;
 }
 
 const UsersPage = async () => {
@@ -14,10 +15,19 @@ const UsersPage = async () => {
   return (
     <div>
      <h1>Users</h1>
-     <p>{new Date().toLocaleTimeString()}</p>
-     <ul>
-        {users.map(users => <li key={users.id}>{users.name}</li>)}
-     </ul>
+     <table className='table table-bordered'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+      {users.map(user => <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td></tr>)}
+      </tbody>
+     </table>
     </div>
   )
 }
